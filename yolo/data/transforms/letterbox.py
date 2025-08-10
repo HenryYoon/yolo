35091cbb38813808b328ce04039a457b47412413
Hee Seung Yun <yoonihg@gmail.com>
2025-08-10 15:14:37 +0900

    🎉 New commit for custom YOLO lib

---
+++ yolo/data/transforms/letterbox.py
@@ -0,0 +1,10 @@
+import torch
+from torchvision.transforms import v2
+
+
+class LetterBox(v2.Transform):
+    def __init__():
+        super().__init__()
+
+    def transform(image:torch.Tensor, new_shape=(640,640)):
+        pass
---
+++ yolo/data/transforms/mosaic.py
@@ -0,0 +1,11 @@
+import random
+
+from torchvision.transforms import v2
+
+
+class MosaicTransform(v2.Transform):
+    def __init__():
+        super().__init__()
+
+    def transform():
+        pass
